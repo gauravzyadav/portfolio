8c22f69cbadc6eaa5dba92571a4dec08db0d1ab3
gauravzyadav <gauravyadav9182@gmail.com>
2025-07-10 05:02:24 +0530

    final navbar update

---
+++ app/components/Header.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <Image />
+        </div>
+    </div>
+  )
+}
+
+export default Header
