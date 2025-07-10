ac9d47333b689cb438c1d6d959c955ca645bdb2b
gauravzyadav <gauravyadav9182@gmail.com>
2025-07-10 05:33:12 +0530

    Added Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,14 +1,35 @@
-import Image from 'next/image'
-import React from 'react'
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-        <div>
-            <Image />
-        </div>
-    </div>
-  )
-}
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col 
+    items-center justify-center gap-4">
+      <div>
+        <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
+      </div>
+      <h3 className="flex items-end gap-2 md:text-2xl mb-3 font-Ovo">
+        Hi! I'm Gaurav Yadav{" "}
+        <Image src={assets.hand_icon} alt="" className=" w-6" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo" >
+        frontend web developer 
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        I am a frontend developer from Varanasi, India with 2 years of experience 
+        in multiple companies like Microsoft, Tesla and Apple.
+      </p>
 
-export default Header
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a href="#contact" className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+        >contact me <Image src={assets.right_arrow_white} alt="" className=" w-4" /></a>
+
+        <a href="/sample-resume.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2" 
+        >my resume <Image src={assets.download_icon} alt="" className=" w-4" /></a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
