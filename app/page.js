f7eae098389ee45808b489673607f028cd3c1e84
gauravzyadav <gauravyadav9182@gmail.com>
2025-07-14 03:09:51 +0530

    added contact

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 'use client'
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Work from "./components/Work";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Navbar/>
       <Header/>
       <About/>
+      <Work/>
+      <Contact/>
     </>
   );
 }
